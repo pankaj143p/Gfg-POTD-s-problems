3487b94011e31fed4d1e05910f9b575206ab3780
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2025-04-29 23:12:43 +0530

    Create given-a-linked-list-of-0s-1s-and-2s-sort-it.java

---
+++ given-a-linked-list-of-0s-1s-and-2s-sort-it.java
@@ -0,0 +1,21 @@
+class Solution {
+    static Node segregate(Node head) {
+        // code here
+        int fre[] = new int[3];
+        Node curr=head;
+        while(curr!=null){
+            fre[curr.data]++;
+            curr=curr.next;
+        }
+        curr=head;
+        int i=0;
+        while(i<3){
+            while(fre[i]-->0){
+                curr.data=i;
+                curr=curr.next;
+            }
+            i++;
+        }
+        return head;
+    }
+}
