f01625ef9eaafbabc63b51941c3a32419a823987
Pankaj Prajapati <114794372+pankaj143p@users.noreply.github.com>
2025-11-11 21:20:44 +0530

    Add function to find shortest common supersequence length
    Implement function to calculate shortest common supersequence length.

---
+++ shortest-common-supersequence.java
@@ -0,0 +1,19 @@
+class Solution
+{
+    //Function to find length of shortest common supersequence of two strings.
+    public static int minSuperSeq(String X,String Y)
+    {
+        int m=X.length();
+        int n=Y.length();
+        int[][] dp = new int[m + 1][n + 1];
+        for (int i = 0; i <= m; i++) {
+            for (int j = 0; j <= n; j++) {
+                if (i == 0 || j == 0) dp[i][j] = 0;
+                else if (X.charAt(i - 1) == Y.charAt(j - 1))
+                    dp[i][j] = 1 + dp[i - 1][j - 1];
+                else dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+        return m + n - dp[m][n];
+    }
+}
